4fe1e749d8df84810c0975bfa6c7c1facad86483
Gustavo <84786540+gus36092@users.noreply.github.com>
2025-11-11 10:00:26 -0600

    Update app.py
    cambio de comentario

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
-    return jsonify({"message": "Gustavo es el mas venenoso del mundo mundial for ever y crallador entonces venenus pudiste"})
+    return jsonify({"message": "Gustavo es el antidoto del mundo mundial for ever y vladi es el crallador venenoso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
