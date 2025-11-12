0bec6bdf96d932ed78becd3b93533e5534d0ea7d
vlady-dev25 <vladimirhernandez@megavision.com.sv>
2025-11-12 08:45:31 -0600

    continous

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
-    return jsonify({"message": "Gustavo es el mas venenoso del mundo mundial for ever y crallador entonces venenus pudiste"})
+    return jsonify({"message": "Vlady-dev25 es el antidotoy Gustavo el mas poison del  mundo mundial for ever y cryador "})
 
 if __name__ == "__main__":
     app.run(debug=True)
